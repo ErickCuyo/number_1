ead3e78bd2c0f0f3f5c92cb0bc776e0b87fbc822
ErickCuyo <99926819+ErickCuyo@users.noreply.github.com>
2022-02-18 21:54:00 -0500

    realizado en hub
    solo aumente mas comentarios

--- ejercicio.py
+++ ejercicio.py
@@ -5,3 +5,5 @@ plot(x,y(x))
 show()
 print(f"lo mejor de la vida es programar")
 print(f"haremos un cohete espacial ")
+# los programas no estan listos por el moemento en ese entonces debemos realizar dichos problemas 
+#  R+  no corre no se que debo hacer por tal razon me estoy especializando en esa rama 
